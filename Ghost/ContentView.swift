a617ddd0a07c08af86df127015c313d07d4382dd
KabirSingh <kabirsingh07@gmail.com>
2024-09-02 12:38:39 -0700

    Initial Commit

---
+++ Ghost/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ghost
+//
+//  Created by Kabir on 9/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ghost/GhostApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhostApp.swift
+//  Ghost
+//
+//  Created by Kabir on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct GhostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
